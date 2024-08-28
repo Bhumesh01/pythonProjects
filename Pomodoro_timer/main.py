bc7509c0a638aae93113d8e66c7fbd375cff1e17
Bhumesh Mahajan <167601736+Bhumesh01@users.noreply.github.com>
2024-08-28 21:57:26 +0530

    Update main.py

--- Pomodoro_timer/main.py
+++ Pomodoro_timer/main.py
@@ -12,38 +12,6 @@ SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 reps = 0
 timer_details = None
-'''
-CANVAS WIDGET:
-allows you to lay the things one on to the other,
-
-NOTE:  ALL GUIs ARE EVENT DRIVEN ie THEY KEEP AN EYE ON ALL THE EVENTS THAT ARE TAKING PLACE
-THE REFRESH THE SCREEN EVERY MOMENT FOR A SPECIFIC ACTION OR AN EVENT.
-HERE IT IS BECAUSE OF mainloop() method that loops through and checks every milli second if something happens.
-thus if we have another loop in our program it won't be able to reach the mainloop and nothing happens
-
-Imagine you're at a party, and there's a group of people chatting and having fun. Each person at the party represents 
-an event, like someone saying something or doing something.
-Now, think of Tkinter as the host of this party. The host's job is to make sure everyone gets to have a good time and 
-that everything runs smoothly.
-In Tkinter, when you create a window and add buttons, text fields, or other elements, it's like setting up different 
-activities at the party. The buttons are like games people can play, and the text fields are places where they can 
-write things.
-Now, the important part: the event loop. This is like the host keeping an eye on everything happening at the party. 
-When someone wants to play a game (like clicking a button), the host needs to be ready to respond and make sure the 
-game runs smoothly.
-If the host gets distracted or busy doing something else (like being stuck in a while loop), they won't be able to 
-pay attention to what's happening at the party. As a result, people might get frustrated because they can't play the 
-games or interact with each other.
-In Tkinter, using a while loop without considering the event loop is like the host getting distracted. The program 
-gets stuck in the loop and doesn't pay attention to what the users are doing (like clicking buttons or typing).
-So, to keep the party (your Tkinter application) running smoothly, you should let the event loop do its job. Instead 
-of using a while loop, you set up event handlers for different actions (like clicking a button), and Tkinter takes 
-care of managing these events while keeping the application responsive and interactive.
-
-however, after() method can be used. it takes the amount of time it should take as an input and the function you tell it 
-to call
-'''
-
 
 # ---------------------------- TIMER RESET ------------------------------- #
 def reset_timer():
